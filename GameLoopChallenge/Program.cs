759c9340b04872d501bff5f7b56591af4e2e49dd
Abigail Evans <cdn_abigail@outlook.com>
2021-10-18 21:18:02 -0300

    Its Alive!!!

--- GameLoopChallenge/Program.cs
+++ GameLoopChallenge/Program.cs
@@ -31,28 +31,28 @@ namespace GameLoopChallenge
         static void Update()
         {
 
-            ConsoleKeyInfo Input = Console.ReadKey(true);
+            ConsoleKeyInfo Input = Console.ReadKey(intercept:true);
             
 
             if(Input.Key == ConsoleKey.W)
             {
                 y -= 1;
-                Console.Write("W");
+                
             }
             else if(Input.Key == ConsoleKey.S)
             {
                 y += 1;
-                Console.Write("S");
+              
             }
             else if (Input.Key == ConsoleKey.A)
             {
                 x -= 1;
-                Console.Write("A");
+                
             }
             else if (Input.Key == ConsoleKey.D)
             {
                 x += 1;
-                Console.Write("D");
+                
             }
             else if (Input.Key == ConsoleKey.Escape)
             {
@@ -61,15 +61,14 @@ namespace GameLoopChallenge
 
         }
 
-        static void PlayerDraw(char player = '*', int x = 0, int y = 0)
+        static void PlayerDraw()
         {
-            if (x >= 0 && y >= 0)
-            {
+
+                Console.Clear();
+                Console.SetCursorPosition(x,y);
+                Console.Write(value: "*");
                
-                Console.SetCursorPosition(x, y);
-                Console.Write(player);
-               
-            }
+            
 
         }
         
